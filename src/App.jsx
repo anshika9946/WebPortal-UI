e431d024b8d776fbfef4e37409aa238b9ad06ac0
Anshika <anshika2011087@akgec.ac.in>
2023-06-21 22:15:59 +0530

    setting up react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Dashboard from "./Components/Dashboard.jsx";
+
+import './App.css';
+
+
+
+const App = () => {
+	return (
+ 
+      <div className="App">
+        <Dashboard />
+      </div>
+
+	);
+}
+
+export default App
