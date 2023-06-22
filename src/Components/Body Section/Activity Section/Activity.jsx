5ea4ecbdbace8ca905ed662882a590f2ef39497b
Anshika <anshika2011087@akgec.ac.in>
2023-06-22 15:14:23 +0530

    creating sidebar

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -8,4 +8,4 @@ const Activity = () => {
   )
 }
 
-export default Actitvity;
+export default Activity;
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Dashboard.css'
+import Top from './Body Section/Top Section/Top'
+import Listing from './Body Section/Listing Section/Listing'
+import Activity from './Body Section/Activity Section/Activity'
+
 
 const Dashboard = () => {
   return (
-    <div className="Dashboard">
-      hello
+    <div className="mainContent">
+      <Top/>
+      
+      <div className="bottom flex">
+        <Listing/>
+        <Activity/>
+        </div> 
     </div>
   )
 }
