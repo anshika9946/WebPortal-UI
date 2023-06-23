06196cd175571a785d8733cf00e6ac49b7492a2c
Anshika <anshika2011087@akgec.ac.in>
2023-06-24 00:02:08 +0530

    sidebar logo and structure updated

--- src/Components/SideBar Section/Sidebar.jsx
+++ src/Components/SideBar Section/Sidebar.jsx
@@ -1,105 +1,95 @@
 import React from 'react'
 import './Sidebar.css'
 
-import logo from '/Assets/logo.svg'
 import { IoMdSpeedometer } from "react-icons/io";
-import { MdDeliveryDining} from "react-icons/md";
+// import { IoMdSpeedometer} from "react-icons/io";
+import { IoHardwareChip} from "react-icons/io5";
+import { BsFillPeopleFill} from "react-icons/bs";
+import { TbLogout2} from "react-icons/tb";
+import { MdAdminPanelSettings} from "react-icons/md";
 import { BsQuestionCircle} from "react-icons/bs";
-import {BsGlobe} from 'react-icons/bs';
+import {MdOutlineLocalPolice} from 'react-icons/md';
 
 const Sidebar = () => {
   return (
     
-      <div className='sideBar grid'>
-{/* <div className="logoDiv flex">
-<img src={logo} alt="Image Name"/>
- <h2>TRACK-PATROL</h2>
-</div> */}
+    <div className='sideBar grid'>
      <div className='logoDiv flex'>
-        <BsGlobe className='icon'/>
-
+        <MdOutlineLocalPolice className='icon'/>
           <h2>TRACK-PATROL</h2>
-
       </div>
 
 
-<div className="menuDiv">
-<h3 className="divTitle">
-QUICK MENU
-</h3>
-<ul className="menuLists grid">
-  <li className="listItem">
-    <a href="#" className='menuLink flex'>
-      <IoMdSpeedometer className="icon"/>
-      <span className="smallText">
-        Manage Bandobast
-        </span>
-    </a>
-  </li>
+    <div className="menuDiv">
+      <h3 className="divTitle">
+      QUICK MENU
+      </h3>
+      <ul className="menuLists grid">
+        <li className="listItem">
+          <a href="#" className='menuLink flex'>
+            <IoMdSpeedometer className="icon"/>
+            <span className="smallText">
+              Manage Bandobast
+              </span>
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="#" className='menuLink flex'>
+            <IoHardwareChip className="icon"/>
+            <span className="smallText">
+              Manage Hardware
+              </span>
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="#" className='menuLink flex'>
+            <BsFillPeopleFill className="icon"/>
+            <span className="smallText">
+              Manage Personnel
+              </span>
+          </a>
+        </li>
 
-  <li className="listItem">
-    <a href="#" className='menuLink flex'>
-      <MdDeliveryDining className="icon"/>
-      <span className="smallText">
-        Manage Hardware
-        </span>
-    </a>
-  </li>
-
-  <li className="listItem">
-    <a href="#" className='menuLink flex'>
-      <IoMdSpeedometer className="icon"/>
-      <span className="smallText">
-        Manage Personnel
-        </span>
-    </a>
-  </li>
-
-</ul>
-</div>
-
-<div className="settingsDiv">
-<h3 className="divTitle">
-SETTINGS
-</h3>
-<ul className="menuLists grid">
-  <li className="listItem">
-    <a href="#" className='menuLink flex'>
-      <IoMdSpeedometer className="icon"/>
-      <span className="smallText">
-       Manage Admin
-        </span>
-    </a>
-  </li>
-
-  <li className="listItem">
-    <a href="#" className='menuLink flex'>
-      <MdDeliveryDining className="icon"/>
-      <span className="smallText">
-        Log Out
-        </span>
-    </a>
-  </li>
-
-
-</ul>
-</div>
-
-<div className="sideBarCard">
-  <BsQuestionCircle className="icon"/>
-  <div className="cardContent">
-  <div className="circle1"></div>
-  <div className="circle2"></div>
-
-  <h3>Help Center</h3>
-  <p>Having trouble in TracPatrol, please contact us for more questions</p>
-
-  <button className="btn">Go to help center</button>
-  </div>
-  </div>
-
-
+      </ul>
     </div>
+
+
+    <div className="settingsDiv">
+      <h3 className="divTitle">
+      SETTINGS
+      </h3>
+      <ul className="menuLists grid">
+        <li className="listItem">
+          <a href="#" className='menuLink flex'>
+            <MdAdminPanelSettings className="icon"/>
+            <span className="smallText">
+            Manage Admin
+              </span>
+          </a>
+        </li>
+        <li className="listItem">
+          <a href="#" className='menuLink flex'>
+            <TbLogout2 className="icon"/>
+            <span className="smallText">
+              Log Out
+              </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+  <div className="sideBarCard">
+    <BsQuestionCircle className="icon"/>
+    <div className="cardContent">
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <h3>Help Center</h3>
+      <p>Having trouble in TracPatrol, please contact us for more questions</p>
+      <button className="btn">Go to help center</button>
+    </div>
+  </div>
+
+</div>
    
   )
 }
