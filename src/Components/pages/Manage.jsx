e81f7ec99a7301de026398438711d31f7a53b06d
Anshika <anshika2011087@akgec.ac.in>
2023-06-28 18:17:32 +0530

    basic bandobast page structure

--- src/Components/pages/Manage.jsx
+++ src/Components/pages/Manage.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import Top from '../Body Section/Top Section/Top'
 import './styles/manage.css'
+import img from '/Assets/Map.png';
 
 const Manage= () => {
   return (
     <div className="manage">
        <Top/>
-      <h2>Statistics</h2>
+      <h2>Ongoing Bandobast</h2>
+      <div class="cards">
+<img src={img} alt="Avatar" />
+
+</div>
+<div class="row">
+    <div class="column">
+      <div class="card">Shift Details</div>
+    </div>
+    <div class="column">
+      <div class="card">Broadcast</div>
+    </div>
+    <div class="column">
+      <div class="card">Status Report</div>
+    </div>
+    <div class="column">
+      <div class="card">Online Check</div>
+    </div>
+</div>
     </div>
   )
 }
